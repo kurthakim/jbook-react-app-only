fb443af73141673eed487f6f714ac69897739a7a
Kurt Hakim <kurthakim@gmail.com>
2022-01-20 00:11:37 +0700

    add error handling

---
+++ test-text.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <h1>Hi There!</h1>;
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+//////////////////////////////////
+const root = document.querySelector('#root');
+root.innerHTML = 'sadad';
+
+////////////////////////////////
+setTimeout(() => {
+  dsadasda.sdadjasdpa();
+}, 100);
